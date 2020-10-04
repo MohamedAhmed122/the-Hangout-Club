554404cd91d2f02131b1f173e785d54fccab811d
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-04 13:58:03 +0700

    Structure the Project And Ready TOOO GOOOO

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import EventDashboard from './app/Pages/EventDashboard/EventDashboard';
 
 function App() {
   return (
     <div className="App">
-      
+      <EventDashboard />
     </div>
   );
 }
---
+++ src/app/Pages/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './StyleEventDashboard.css'
+
+export default function EventDashboard() {
+    return (
+        <div className='event-dashboard'>
+            <div className='sidebar-left'>
+                <h1>hello I am sidebar Left</h1>
+            </div>
+            <div className='main'>
+            <h1>hello I am  the main page</h1>
+            </div>
+            <div className='sidebar-right'>
+                <h1>hello I am sidebar Left</h1>
+            </div>
+        </div>
+    )
+}
