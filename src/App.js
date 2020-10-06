c9f6f369d6344186708b1f800d69dda8413829c7
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-06 13:31:21 +0700

    Working on The Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Navbar from './app/layouts/Header/Header';
 import EventDashboard from './app/Pages/EventDashboard/EventDashboard';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <EventDashboard />
     </div>
   );
---
+++ src/app/layouts/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {  Search } from 'semantic-ui-react'
+import './StyleHeader.css'
+
+export default function Navbar() {
+    return (
+     <div className='header'>
+         <div className='header-right'>
+             <img src='https://de-production.imgix.net/colors/browser/de5754.jpg?fit=fill&bg=ffffff&fm=jpeg&auto=format&lossless=1' alt='' />
+             <Search placeholder='Search Events'/>
+         </div>
+         <div className='header-middle'>
+             <button className='link current' >
+                 Home
+            </button>
+            <button className='link current' >
+                 Events
+            </button>
+            <button className='link current' >
+                 Community
+            </button>
+             
+         </div>
+         <div className='header-left'>
+            <img src='https://c.files.bbci.co.uk/6FF7/production/_103536682_gettyimages-973397370.jpg' alt='' />
+            <div>Mohamed Youssef</div>
+         </div>
+     </div>
+    )
+}
